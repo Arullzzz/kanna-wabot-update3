f11d02d4a7891ae3d4a2f0970f2e58d35a458786
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-31 16:22:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ function pickRandom(list) {
 // Global Staff
 global.DeveloperMode = 'false' //Buat Mode True False Moderator
 global.linkGC = ['https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT'] // ganti jadi group lu
-global.owner = ['62831433937633', '6283143393763' ,'6288217277973','62831433937630','6285704954639', '62895336282144', '6285712420674'] // Put your number here
-global.mods = ['6285231651905'] // Want some help?
+global.owner = ['6285822347348', '6285822347348' ,'6285822347348','6285822347348','6285822347348', '6285822347348', '6285822347348'] // Put your number here
+global.mods = ['6285822347348'] // Want some help?
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -69,12 +69,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '🌙 Ƙαnnα вσт\nwa.me/6283143393763\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @ppiowy_\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
+global.packname = '🌙 ƘArullBotz\nwa.me/6285822347348\n\n\n𝙱𝚢 𝙻𝚎𝚝𝚝𝚊 あ\n\n\nFollow IG: @sahrulwara_____\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n.'
 global.author = '۰ ⸼'
 
 // BOT WM
-global.botwm = '▧ Kαnnα BOT | © ℓetta - sαmα' //UBAH SETERAHMU
-global.wm = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Ƙαnnα - chαn˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.botwm = '▧ ArullBotz | © Arull' //UBAH SETERAHMU
+global.wm = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Ƙαrull˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
 global.botdate = `❏ ${wktuh} H  ${wktum} M  ${wktus} S\n❏ ${week} ${date}`
 
 // LOGO
@@ -104,7 +104,7 @@ global.bs = scraper
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 // tingkat kesulitan, semakin tinggi semakin susah
-global.multiplier = 28 // The higher, The harder levelup
+global.multiplier = 28888 // The higher, The harder levelup
 
 //*****************PEMBATAS*********************
 // JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
